84d19b71766d9a6cb9b50dcf2cd8d3605222208e
Olatoyan <golatoyan@gmail.com>
2023-04-17 20:28:05 +0100

    fixed some errors

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ const cardExpiryMonth = document.querySelector(".details__expiry--month");
 const cardExpiryYear = document.querySelector(".details__expiry--year");
 const backCardCvc = document.querySelector(".back__number");
 const errorName = document.querySelector(".error__name");
+const errorNameShort = document.querySelector(".error__name-short");
 const errorNumber = document.querySelector(".error__number");
-const errorExpiry = document.querySelector(".error__expiry");
+const errorNumberIncomplete = document.querySelector(".error__num");
+const errorMonth = document.querySelector(".error__month");
+const errorYear = document.querySelector(".error__year");
 const errorCvc = document.querySelector(".error__cvc");
+const errorCvcNum = document.querySelector(".error__cvc-num");
 const form = document.querySelector(".form");
 const formBox = document.querySelector(".form__box");
 const success = document.querySelector(".success");
@@ -85,13 +89,19 @@ cvc.addEventListener("input", function () {
 
 function validateForm(e) {
   const nameCard = cardName.value.trim();
-  if (!/^[a-zA-z\s]*$/.test(nameCard)) {
+  if (!/^[a-zA-z\s]*$/.test(nameCard) || nameCard === "") {
     errorName.style.display = "block";
+    cardName.style.border = "1px solid #ff5252";
+    return false;
+  } else if (nameCard.length < 7) {
+    errorName.style.display = "none";
+    errorNameShort.style.display = "block";
     cardName.style.border = "1px solid #ff5252";
     return false;
   } else {
     // CHANGING THE STYLES BACK TO NORMAL
     errorName.style.display = "none";
+    errorNameShort.style.display = "none";
     cardName.style.border = "1px solid #0000003b";
   }
 
@@ -101,27 +111,47 @@ function validateForm(e) {
     errorNumber.style.display = "block";
     cardNumber.style.border = "1px solid #ff5252";
     return false;
+  } else if (numberCard.length !== 16) {
+    errorNumber.style.display = "none";
+    errorNumberIncomplete.style.display = "block";
+    cardNumber.style.border = "1px solid #ff5252";
+    return false;
   } else {
     // CHANGING THE STYLES BACK TO NORMAL
     errorNumber.style.display = "none";
+    errorNumberIncomplete.style.display = "none";
     cardNumber.style.border = "1px solid #0000003b";
   }
 
   //
 
   // VALIDATNG THE YEAR TO CONTAIN ONLY NUMBERS
-  const monthExpiry = expiryMonth.value.trim();
-  const yearExpiry = expiryYear.value.trim();
+  const monthExpiry = Number(expiryMonth.value.trim());
+  const yearExpiry = Number(expiryYear.value.trim());
 
   if (!/^\d+$/.test(monthExpiry) || !/^\d+$/.test(yearExpiry)) {
-    errorExpiry.style.display = "block";
+    errorMonth.style.display = "block";
+    errorYear.style.display = "block";
     expiryMonth.style.border = "1px solid #ff5252";
     expiryYear.style.border = "1px solid #ff5252";
-
+    console.log(monthExpiry > 45);
+    return false;
+  } else if (monthExpiry < 1 || monthExpiry > 12) {
+    errorYear.style.display = "none";
+    errorMonth.style.display = "block";
+    expiryMonth.style.border = "1px solid #ff5252";
+    expiryYear.style.border = "1px solid #14141456";
+    return false;
+  } else if (yearExpiry < 23 || yearExpiry > 28) {
+    errorMonth.style.display = "none";
+    errorYear.style.display = "block";
+    expiryMonth.style.border = "1px solid #14141456";
+    expiryYear.style.border = "1px solid #ff5252";
     return false;
   } else {
     // CHANGING THE STYLES BACK TO NORMAL
-    errorExpiry.style.display = "none";
+    errorMonth.style.display = "none";
+    errorYear.style.display = "none";
     expiryMonth.style.border = "1px solid #0000003b";
     expiryYear.style.border = "1px solid #0000003b";
   }
@@ -134,9 +164,15 @@ function validateForm(e) {
     cvc.style.border = "1px solid #ff5252";
 
     return false;
+  } else if (cvcNumber.length !== 3) {
+    errorCvc.style.display = "none";
+    errorCvcNum.style.display = "block";
+    return false;
   } else {
     // CHANGING THE STYLES BACK TO NORMAL
     errorCvc.style.display = "none";
+    errorCvcNum.style.display = "none";
+
     cvc.style.border = "1px solid #0000003b";
   }
 
